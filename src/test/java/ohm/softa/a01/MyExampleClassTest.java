de844309d774474027c01673be6e062bf69f9b05
adiko <dikodam@googlemail.com>
2019-03-25 17:11:16 +0100

    prettier test method

--- src/test/java/ohm/softa/a01/MyExampleClassTest.java
+++ src/test/java/ohm/softa/a01/MyExampleClassTest.java
@@ -10,6 +10,15 @@ class MyExampleClassTest {
     @Test
     void testMultiply() {
         IntStream.range(-5, 5)
-                .forEach(i -> assertEquals(i * (i - 1), new MyExampleClass().multiply(i, i - 1)));
+                 .forEach(i -> assertEquals(i * (i - 1), new MyExampleClass().multiply(i, i - 1)));
+    }
+    
+    @Test
+    void testMultiplyPretty() {
+        MyExampleClass testee = new MyExampleClass();
+        assertAll(
+            IntStream.range(-5, 5)
+                     .mapToObj(i -> () -> assertEquals(i * (i - 1), testee.multiply(i, i - 1)))
+        );
     }
 }
